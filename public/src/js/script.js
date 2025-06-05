5f143c3f9f6415ec4861a5ae908f77d07f4e0a47
Mateus Macedo <mateusmacedobatista05@gmail.com>
2025-06-05 18:11:40 -0300

    criação do slide - estilização - lógica no js

--- public/src/js/script.js
+++ public/src/js/script.js
@@ -17,3 +17,43 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+let indiceSlide = 1; // Começa no primeiro slide
+mostrarSlide(indiceSlide);
+
+// Função para avançar/retroceder N slides
+function mudarSlide(n) {
+    mostrarSlide(indiceSlide += n);
+}
+
+// Função para mostrar um slide específico
+function mostrarSlide(n) {
+    let i;
+    let slides = document.getElementsByClassName("meu-slide");
+    let pontos = document.getElementsByClassName("ponto");
+
+    if (n > slides.length) { // Se passar do último slide, volta para o primeiro
+        indiceSlide = 1;
+    }
+    if (n < 1) { // Se voltar antes do primeiro, vai para o último
+        indiceSlide = slides.length;
+    }
+
+    // Esconde todos os slides
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    // Remove a classe "ativo" de todos os pontos
+    for (i = 0; i < pontos.length; i++) {
+        pontos[i].className = pontos[i].className.replace(" ativo", "");
+    }
+
+    // Mostra o slide atual e marca o ponto como ativo
+    if (slides.length > 0) { // Verifica se existem slides
+        slides[indiceSlide - 1].style.display = "block";
+    }
+    if (pontos.length > 0) { // Verifica se existem pontos
+        pontos[indiceSlide - 1].className += " ativo";
+    }
+}
